703863cded2aff1bbce31da646fdf6a90d3f41cb
Faaris Khan <tdv2188@autuni.ac.nz>
2022-09-24 15:37:31 +1200

    Added code to app.js to deploy heroku

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('This is the initial setup for Group 9 CISE SPEED Project Stream 50 - Successfully Deployed!'));
+//app.get('/', (req, res) => res.send('This is the initial setup for Group 9 CISE SPEED Project Stream 50 - Successfully Deployed!'));
+
+app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
+})
 
 const port = process.env.PORT || 5000;
 
