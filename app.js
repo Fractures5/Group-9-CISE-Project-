1c95ee2db27d127d6232fe169909f6ecc081db47
Faaris Khan <tdv2188@autuni.ac.nz>
2022-09-24 16:24:37 +1200

    Updated app.js code

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 // app.js
 require('dotenv').config({ path: "./env"});
 const express = require('express');
-//const connectDB = require('./config/db');
-//var cors = require('cors');
+const connectDB = require('./config/db');
+var cors = require('cors');
 const app = express();
 const path =  require('path');
 const { response } = require('express');
 
 // Connect Database
-//connectDB();
-
-//app.get('/', (req, res) => res.send('This is the initial setup for Group 9 CISE SPEED Project Stream 50 - Successfully Deployed!'));
+connectDB();
 
 app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
 app.get("*", (req, res) => {
