172c287fcff18c811aba7277eb56127bfd124625
Faaris Khan <tdv2188@autuni.ac.nz>
2022-09-24 16:03:31 +1200

    Added code to app.js, .env, and others

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // app.js
 require('dotenv').config({ path: "./env"});
 const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+//const connectDB = require('./config/db');
+//var cors = require('cors');
 const app = express();
 const path =  require('path');
 const { response } = require('express');
 
 // Connect Database
-connectDB();
+//connectDB();
 
 //app.get('/', (req, res) => res.send('This is the initial setup for Group 9 CISE SPEED Project Stream 50 - Successfully Deployed!'));
 
 app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "cise-speed-app", "build", "App.js"))
+    res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
