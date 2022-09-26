5d6f588fa71c68e64a6f53c39d4ec8b6a27aa656
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-09-26 18:25:15 +1300

    Added base submit article code in suggest-article-page.js

--- cise-speed-app/src/pages/suggest-article-page.js
+++ cise-speed-app/src/pages/suggest-article-page.js
@@ -1,1 +1,17 @@
 // This is suggest article page which will have the form for the submitter to suggest a new article/practice.
+
+import React from "react";
+import SubmissionForm from "../components/SubmissionForm.js";
+ 
+const SubmitArticle = () =>  {
+    return (
+    <div>
+        <h2>Submit Article</h2>
+            <p>This will be a form to allow submitters to submit an article for possible 
+                inclusion in our SPEED repo.</p>
+            <SubmissionForm/>
+    </div>
+    );
+}
+ 
+export default SubmitArticle;
