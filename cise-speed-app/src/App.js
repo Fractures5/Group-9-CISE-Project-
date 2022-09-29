b8ecbd345e0d3e6bcc7918d0065e5654ce23a292
Anuk Silva <vwv9667@autuni.ac.nz>
2022-09-30 04:30:57 +1300

    On app load, will successfully load into contents of home-page.js and Home navbar redirectsproperly to app-load page

--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -9,13 +9,13 @@ const App = () => {
       <div>
         <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
          <ul className="header">
-          <li><NavLink exact to="/Home">Home</NavLink></li>
+          <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink exact to="/Home">Suggest Article</NavLink></li>
           <li><NavLink exact to="/Home">Backend</NavLink></li>
           <li><NavLink to="/Home">Select the Practice</NavLink></li>
         </ul>
         <div className="content">
-          <Route exact path="/Home" component={Home} />
+          <Route exact path="/" component={Home} />
         </div>
       </div>
     </Router>
