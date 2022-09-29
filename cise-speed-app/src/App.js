189f3d21512e56a3914b7f4515bcbc219d48e2ef
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-09-29 22:29:24 +1300

    Updated App.js in attempt to show page (currently shows blank page

--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react"
+import { Route, NavLink, BrowserRouter as Router} from "react-router-dom";
+
+import SuggestArticle from "./pages/suggest-article-page";
 import './App.css';
+
 //App.js
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test test test
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/SuggestArticle">Suggest Article</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SuggestArticle">Select the Practice</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route path="/SuggestArticle" component={SuggestArticle} />
+        </div>
+      </div>
+    </Router>
   );
 }
-
 export default App;
+
+
--- cise-speed-app/src/pages/suggest-article-page.js
+++ cise-speed-app/src/pages/suggest-article-page.js
@@ -1,7 +1,7 @@
 // This is suggest article page which will have the form for the submitter to suggest a new article/practice.
 
-import React from "react";
-import SuggestArticleForm from "../components/suggest-article-form.js";
+// import React from "react";
+// import SuggestArticleForm from "../components/suggest-article-form.js";
  
 const SuggestArticle = () =>  {
     return (
@@ -9,9 +9,9 @@ const SuggestArticle = () =>  {
         <h2>Submit Article</h2>
             <p>This will be a form to allow submitters to submit an article for possible 
                 inclusion in our SPEED repo.</p>
-            <SuggestArticleForm/>
+            {/* <SuggestArticleForm/> */}
     </div>
     );
-}
+};
  
 export default SuggestArticle;
