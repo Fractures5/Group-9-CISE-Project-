162401b7616fae24ea1c4ac9b85ebd367c9a6d7a
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-09-30 15:39:34 +1300

    Create database model for submitting by adding suggest-article-model.js

---
+++ models/suggest-article-model.js
@@ -0,0 +1,40 @@
+// models/suggest-article-model.js
+
+const mongoose = require('mongoose');
+
+const SuggestArticleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  journalName: {
+    type: String,
+    required: true
+  },
+  publciationYear: {
+    type: Number,
+    required: true
+  },
+  articleVolume: {
+    type: Number,
+  },
+  articleNumber: {
+    type: Number,
+  },
+  articlePages: {
+    type: Number,
+  },
+  articleDOI: {
+    type: String,
+  },
+  selectedSEPractice: {
+    type: String,
+  }
+});
+
+//Will add bibtext file upload text after research
+module.exports = ArticleSuggestion = mongoose.model('book', SuggestArticleSchema);
