434ed07d98ebc6c9bb1d73ab1fbd63fda269751e
Anuk Silva <vwv9667@autuni.ac.nz>
2022-09-30 19:29:09 +1300

    Loading pages that do not exist will redirect the user to the error 404 page

--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
+import { Route, NavLink, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 import Home from "./pages/home-page.js";
+import NotFoundPage from "./pages/404";
 import "./App.css";
 const App = () => {
   return (
     <Router>
       <div>
         <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-         <ul className="header">
-          <li><NavLink exact to="/">Home</NavLink></li>
-          <li><NavLink exact to="/SuggestArticle">Suggest Article</NavLink></li>
-          <li><NavLink exact to="/SubmitArticle">Backend</NavLink></li>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/SuggestArticle">
+              Suggest Article
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/SubmitArticle">
+              Backend
+            </NavLink>
+          </li>
           {/* <li><NavLink to="/Home">Select the Practice</NavLink></li> */}
         </ul>
         <div className="content">
           <Route exact path="/" component={Home} />
+          <Route exact path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
         </div>
       </div>
     </Router>
