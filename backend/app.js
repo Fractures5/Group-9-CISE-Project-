c1b2ea17ba45805829924973f686a4fd9b07da41
Anuk Silva <vwv9667@autuni.ac.nz>
2022-10-03 01:13:48 +1300

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,35 @@
 // app.js
-require('dotenv').config({ path: "./env"});
+//require('dotenv').config({ path: "./env"});
 const express = require('express');
 const connectDB = require('../config/db');
 var cors = require('cors');
+//const path =  require('path');
+//const { response } = require('express');
+
+// routes
+const suggestArticles = require('../routes/api/articleRoutes');
+const backendArticles = require('../routes/api/backendArticleRoutes');
 const app = express();
-const path =  require('path');
-const { response } = require('express');
 
 // Connect Database
 connectDB();
 
-app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
-})
+//cors
+app.use(cors({origin: true, credentials: true}));
+
+// Init middleware
+app.use(express.json({extended: false}));
+
+// app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
+// })
+
+app.get('/', (req, res) => res.send('Testing database submission form'));
+
+// Use routes
+app.use('/api/articleRoutes', suggestArticles);
+app.use('/api/backendArticleRoutes', backendArticles);
 
 const port = process.env.PORT || 8082;
 
