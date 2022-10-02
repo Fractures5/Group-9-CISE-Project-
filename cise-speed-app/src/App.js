2af7d3575b949f467a457890fed2608b1c0e2f8e
Faaris Khan <tdv2188@autuni.ac.nz>
2022-10-03 00:09:51 +1300

    Adjusted App.js to avoid merge issues into main

--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -1,30 +1,4 @@
-import React from "react"
-import { Route, NavLink, BrowserRouter as Router} from "react-router-dom";
-
-import BackendArticle from "./pages/backend-article-page";
-import './App.css';
-
-//App.js
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-        <ul className="header">
-          <li>
-            <NavLink exact to="/BackendArticle">Submit an Article</NavLink>
-          </li>
-        </ul>
-        <div className="content">
-          <Route path="/BackendArticle" component={BackendArticle} />
-        </div>
-      </div>
-    </Router>
-  );
-}
-export default App;
-
-/*import React from "react";
+import React from "react";
 import { Route, NavLink, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 import Home from "./pages/home-page.js";
@@ -58,4 +32,4 @@ const App = () => {
   );
 }
 
-export default App;*/
+export default App;
