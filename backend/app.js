945935a9dd9b1dd839cb4729a3c6cd1b93024b61
Faaris Khan <tdv2188@autuni.ac.nz>
2022-10-02 23:59:02 +1300

    Have app.js in backend working

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+// app.js
+//require('dotenv').config({ path: "./env"});
+const express = require('express');
+const connectDB = require('../config/db');
+var cors = require('cors');
+//const path =  require('path');
+//const { response } = require('express');
+
+// routes
+const articles = require('../routes/api/backendArticleRoutes');
+const app = express();
+
+// Connect Database
+connectDB();
+
+//cors
+app.use(cors({origin: true, credentials: true}));
+
+// Init middleware
+app.use(express.json({extended: false}));
+
+// app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
+// })
+
+app.get('/', (req, res) => res.send('Testing database submission form'));
+
+// Use routes
+app.use('/api/backendArticleRoutes', articles);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -23,3 +23,39 @@ const App = () => {
   );
 }
 export default App;
+
+/*import React from "react";
+import { Route, NavLink, BrowserRouter as Router, Redirect } from "react-router-dom";
+
+import Home from "./pages/home-page.js";
+import NotFoundPage from "./pages/404";
+import "./App.css";
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li><NavLink exact to = "/">Home</NavLink></li>
+          <li>
+            <NavLink to = "/SuggestArticle">
+              Suggest Article
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to = "/SubmitArticle">
+              Backend
+            </NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route exact path='/' component={Home} />
+          <Route exact path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;*/
