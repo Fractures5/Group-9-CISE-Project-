ceae2ce2959e4ad456ed30ed7a6da635fd6298c7
Anuk Silva <vwv9667@autuni.ac.nz>
2022-10-03 01:45:44 +1300

    Stable build of the application with the feature branches merged to main

--- cise-speed-app/src/App.js
+++ cise-speed-app/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Route, NavLink, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 import Home from "./pages/home-page.js";
+import SuggestArticle from "./pages/suggest-article-page.js";
 import NotFoundPage from "./pages/404";
+import BackendArticle from "./pages/backend-article-page.js";
 import "./App.css";
 const App = () => {
   return (
@@ -10,22 +12,16 @@ const App = () => {
       <div>
         <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
         <ul className="header">
-          <li><NavLink exact to = "/">Home</NavLink></li>
-          <li>
-            <NavLink to = "/SuggestArticle">
-              Suggest Article
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to = "/SubmitArticle">
-              Backend
-            </NavLink>
-          </li>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/SuggestArticleForm">Suggest Article</NavLink></li>
+          <li><NavLink to="/BackendArticleForm">Backend</NavLink></li>
         </ul>
         <div className="content">
-          <Route exact path='/' component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/SuggestArticleForm" component={SuggestArticle} />
+          <Route exact path="/BackendArticleForm" component={BackendArticle} />
           <Route exact path="/404" component={NotFoundPage} />
-          <Redirect to="/404" />
+          {/* <Redirect to="/404" /> */}
         </div>
       </div>
     </Router>
