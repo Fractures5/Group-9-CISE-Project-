2e6648d7e4618428b86bef20ac08337bb78b783c
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-10-13 21:12:56 +1300

    Updated article.js dummy data in attempts to view DB data in home page table

--- cise-speed-app/src/components/show-db-articles.js
+++ cise-speed-app/src/components/show-db-articles.js
@@ -64,43 +64,9 @@ class showDBArticles extends Component {
     return (
       <div className="showDBArticles">
         <div className="container">
-          <div className="row">
-            <div className="col-md-10 m-auto">
-              <br /> <br />
-              <Link to="/" className="btn btn-outline-warning float-left">
-                  Show Book List
-              </Link>
-            </div>
-            <br />
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Book's Record</h1>
-              <p className="lead text-center">
-                  View Book's Info
-              </p>
-              <hr /> <br />
-            </div>
-          </div>
           <div>
             { BookItem }
           </div>
-
-          <div className="row">
-            <div className="col-md-6">
-              <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={this.onDeleteClick.bind(this,book._id)}>Delete Book</button><br />
-            </div>
-
-            <div className="col-md-6">
-              <Link to={`/edit-book/${article._id}`} className="btn btn-outline-info btn-lg btn-block">
-                    Edit Book
-              </Link>
-              <br />
-            </div>
-
-          </div>
-            {/* <br />
-            <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Book</button>
-            <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Book</button> */}
-
         </div>
       </div>
     );
