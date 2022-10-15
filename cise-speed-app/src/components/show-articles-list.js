5011b322e8d2afeb283caa613d1468554e46fa24
Anuk Silva <vwv9667@autuni.ac.nz>
2022-10-15 19:39:50 +1300

    Created dynamic table for viewing MongoDB articles with basic sorting

--- cise-speed-app/src/components/show-articles-list.js
+++ cise-speed-app/src/components/show-articles-list.js
@@ -1,153 +1,249 @@
-import React, { Component } from 'react';
-import { useState } from "react";
-import '../App.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import ArticlesTable from './articles-table';
 
-class ShowArticlesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-    };
+// App.js
 
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    // this.sortSEPractice = this.sortSEPractice.bind(this);
-  }
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 
-  handleKeyUp() {
-    console.log("getting triggerd");
+import Table from "../components/evidence-table.js";
+import "../App.css";
 
-    var input, filter, table, tr, td, i, txtValue;
-      input = document.getElementById("searchTitle");
-      filter = input.value.toUpperCase();
-      table = document.getElementById("articleTable");
-      tr = table.getElementsByTagName("tr");
-      for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-          txtValue = td.textContent || td.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
-        }       
-      }
-    console.log("reads bottom of function ");
+function App() {
+  const [data, setData] = useState([]);
 
-  }
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Articles",
+        columns: [
+          {
+            Header: "Title",
+            accessor: "title",
+          },
+          {
+            Header: "Author",
+            accessor: "author",
+          },
+          {
+            Header: "Journal Name",
+            accessor: "journalName",
+          },
+          {
+            Header: "Publication Year",
+            accessor: "publicationYear",
+          },
+          {
+            Header: "Claim",
+            accessor: "claim",
+          },
+          {
+            Header: "SEPractice",
+            accessor: "sePractice",
+          },
+          {
+            Header: "Result of Evidence",
+            accessor: "resultOfEvidence",
+          },
+          {
+            Header: "Research Type",
+            accessor: "researchType",
+          },
+          {
+            Header: "Type of Participant",
+            accessor: "typeOfParticipant",
+          },
+        ],
+      },
+    ],
+    []
+  );
 
-  // sortSEPractice() {
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("http://localhost:8082/api/backendArticleRoutes");
+      setData(result.data);
+    })();
+  }, []);
 
-  //   console.log("triggering sortSEPractice function");
-
-  //   var input, filter, table, tr, td, i;
-  //   input = document.getElementById("sePracticeList");
-  //   filter = input.value.toUpperCase();
-  //   table = document.getElementById("articleTable");
-  //   tr = table.getElementsByTagName("tr");
-  //   for (i = 0; i < tr.length; i++) {
-  //     td = tr[i].getElementsByTagName("td")[0];
-  //     if (td) {
-  //       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-  //         tr[i].style.display = "";
-  //       } else {
-  //         tr[i].style.display = "none";
-  //       }
-  //     }       
-  //   }
-  //   console.log("reads bottom of sortSEPractice function ");
-  // }
-
-  componentDidMount() {
-    axios
-      .get('http://localhost:8082/api/backendArticleRoutes')
-      .then(res => {
-        this.setState({
-            articles: res.data
-        })
-      })
-      .catch((err) => {
-        console.log("Error from ShowArticleList");
-      });
-  }
- 
-
-  render() {
-    // const articles = this.state.articles;
-    // console.log("PrintBook: " + articles);
-    // let articleList;
-
-    // if(!articles) {
-    //     articleList = "there is no book record!";
-    // } else {
-    //     articleList = articles.map((article, k) => (
-    //       <ArticlesTable article={article} key={k} />
-    //     ));
-    // }
-    const articles = this.state.articles;
-    return (
-      <div className="ShowBookList">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="header-center">
-                <h2 className="display-4 text-center">Articles List</h2>
-              </div>
-              {/* <select id="sePracticeList" onChange={this.sortSEPractice()} class='form-control'>
-                <option>TDD</option>
-                <option>Mob Programming</option>
-                <option>Agile Programming</option>
-              </select> */}
-              <input type="text" id="searchTitle" onKeyUp={this.handleKeyUp} placeholder="Search for names.." title="Type in a name"></input>
-              <table id="articleTable">
-                <tr>
-                  <th>Title</th>
-                  <th>Author</th>
-                  <th>Journal Name</th>
-                  <th>Publication Year</th>
-                  <th>Claim</th>
-                  <th>SE Practice</th>
-                  <th>Evidence</th>
-                  <th>Research Type</th>
-                  <th>Type of Participant</th>
-                </tr>
-              </table>
-            </div>
-          </div>
-          {/* <div className="list">{articleList}</div> */}
-
-            {articles.map((article) => (
-              <div className ="searchArticles">
-                <div className="tableContainer">
-      
-                <table id="articleTable">
-                  <tr>
-                    <td>{article.title}</td>
-                    <td>{article.author}</td>
-                    <td>{article.journalName}</td>
-                    <td>{article.publicationYear}</td>
-                    <td>{article.claim}</td>
-                    <td>{article.sePractice}</td>
-                    <td>{article.resultOfEvidence}</td>
-                    <td>{article.researchType}</td>
-                    <td>{article.typeOfParticipant}</td>
-                  </tr>
-                {/* <br></br> */}
-              </table>
-              </div>
-            </div>
-            
-            ))}
-
-        </div>
-      </div>
-    );
-
-    
-  }
+  return (
+    <div className="App">
+      <Table columns={columns} data={data} />
+    </div>
+  );
 }
 
-export default ShowArticlesList;
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { useState } from "react";
+// import '../App.css';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import ArticlesTable from './articles-table';
+
+// class ShowArticlesList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       articles: [],
+//     };
+
+//     this.handleKeyUp = this.handleKeyUp.bind(this);
+//     this.sortSEPractice = this.sortSEPractice.bind(this);
+//   }
+
+//   handleKeyUp() {
+//     console.log("getting triggerd");
+
+//     var input, filter, table, tr, td, i, txtValue;
+//       input = document.getElementById("searchTitle");
+//       filter = input.value.toUpperCase();
+//       table = document.getElementById("articleTable");
+//       tr = table.getElementsByTagName("tr");
+//       for (i = 0; i < tr.length; i++) {
+//         td = tr[i].getElementsByTagName("td")[0];
+//         if (td) {
+//           txtValue = td.textContent || td.innerText;
+//           if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             tr[i].style.display = "";
+//           } else {
+//             tr[i].style.display = "none";
+//           }
+//         }       
+//       }
+//     console.log("reads bottom of function ");
+
+//   }
+
+//   sortSEPractice() {
+
+//     console.log("triggering sortSEPractice function");
+
+//     var input, filter, table, tr, td, i;
+//     input = document.getElementById("sePracticeList");
+//     filter = input.value.toUpperCase();
+//     table = document.getElementById("articleTable");
+//     tr = table.getElementsByTagName("tr");
+//     for (i = 0; i < tr.length; i++) {
+//       td = tr[i].getElementsByTagName("td")[0];
+//       if (td) {
+//         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//           tr[i].style.display = "";
+//         } else {
+//           tr[i].style.display = "none";
+//         }
+//       }       
+//     }
+//     console.log("reads bottom of sortSEPractice function ");
+//   }
+
+//   componentDidMount() {
+//     axios
+//       .get('http://localhost:8082/api/backendArticleRoutes')
+//       .then(res => {
+//         this.setState({
+//             articles: res.data
+//         })
+//       })
+//       .catch((err) => {
+//         console.log("Error from ShowArticleList");
+//       });
+//   }
+ 
+
+//   render() {
+//     // const articles = this.state.articles;
+//     // console.log("PrintBook: " + articles);
+//     // let articleList;
+
+//     // if(!articles) {
+//     //     articleList = "there is no book record!";
+//     // } else {
+//     //     articleList = articles.map((article, k) => (
+//     //       <ArticlesTable article={article} key={k} />
+//     //     ));
+//     // }
+//     const articles = this.state.articles;
+//     return (
+//       <div className="ShowBookList">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-md-12">
+//               <div className="header-center">
+//                 <h2 className="display-4 text-center">Articles List</h2>
+//               </div>
+//               <select id="sePracticeList" onChange={this.sortSEPractice} class='form-control'>
+//                 <option>TDD</option>
+//                 <option>Mob Programming</option>
+//                 <option>Agile Programming</option>
+//               </select>
+//               <input type="text" id="searchTitle" onKeyUp={this.handleKeyUp} placeholder="Search for names.." title="Type in a name"></input>
+//               <table id="articleTable">
+//                 <tr>
+//                   <th>Title</th>
+//                   <th>Author</th>
+//                   <th>Journal Name</th>
+//                   <th>Publication Year</th>
+//                   <th>Claim</th>
+//                   <th>SE Practice</th>
+//                   <th>Evidence</th>
+//                   <th>Research Type</th>
+//                   <th>Type of Participant</th>
+//                 </tr>
+//               </table>
+//             </div>
+//           </div>
+//           {/* <div className="list">{articleList}</div> */}
+
+//             {articles.map((article) => (
+//               <div className ="searchArticles">
+//                 <div className="tableContainer">
+      
+//                 <table id="articleTable">
+//                   <tr>
+//                     <td>{article.title}</td>
+//                     <td>{article.author}</td>
+//                     <td>{article.journalName}</td>
+//                     <td>{article.publicationYear}</td>
+//                     <td>{article.claim}</td>
+//                     <td>{article.sePractice}</td>
+//                     <td>{article.resultOfEvidence}</td>
+//                     <td>{article.researchType}</td>
+//                     <td>{article.typeOfParticipant}</td>
+//                   </tr>
+//                 {/* <br></br> */}
+//               </table>
+//               </div>
+//             </div>
+            
+//             ))}
+
+//         </div>
+//       </div>
+//     );
+
+    
+//   }
+// }
+
+// export default ShowArticlesList;
