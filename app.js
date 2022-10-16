bae91918559d8998a070a575666367b9c954ff32
Faaris Khan <tdv2188@autuni.ac.nz>
2022-10-16 16:01:33 +1300

    Deploying working local application to Heroku

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
 })
 
-app.get('/', (req, res) => res.send('Testing database submission form'));
+//app.get('/', (req, res) => res.send('Testing database submission form'));
 
 // Use routes
 app.use('/api/articleRoutes', suggestArticles);
