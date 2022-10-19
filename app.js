73c95c556238ac09c95316ba27ac9b37cc6eab6e
Faaris Khan <tdv2188@autuni.ac.nz>
2022-10-20 00:01:08 +1300

    Testing article table on Heroku

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // app.js File
-//require('dotenv').config({ path: "./env"});
+require('dotenv').config({ path: "./env"});
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
-//const path =  require('path');
-//const { response } = require('express');
+const path =  require('path');
+const { response } = require('express');
 
 // routes
 const suggestArticles = require('./routes/api/articleRoutes');
@@ -20,10 +20,10 @@ app.use(cors({origin: true, credentials: true}));
 // Init middleware
 app.use(express.json({extended: false}));
 
-/*app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
+app.use(express.static(path.join(__dirname, "./cise-speed-app/build")))
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "cise-speed-app", "build", "index.html"))
-})*/
+})
 
 //app.get('/', (req, res) => res.send('Testing database submission form'));
 
